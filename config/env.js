e22e19d57dfb93c9dd15a466a163c0e6a23faeae
skypanda100 <236520700@qq.com>
2018-09-23 11:45:38 +0800

    1. 更改config

--- config/env.js
+++ config/env.js
@@ -1,4 +1,6 @@
 export default {
-    host: 'http://localhost:3000',
-    imageHost: 'http://localhost:12300'
+    // host: 'http://localhost:90',
+    // imageHost: 'http://localhost:12300'
+    host: 'http://47.94.165.17:90',
+    imageHost: 'http://47.94.165.17:84'
 };
